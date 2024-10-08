e01e6434875abff29566c2b0e8eff9f0c5a18eec
Nyi Ye Han <nyiyehan@gmail.com>
2024-10-08 10:04:49 +0700

    [Feature] add UILable in view controller

--- GitHubTesting/ViewController.swift
+++ GitHubTesting/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let image = UIImage(systemName: "circle")
         // Do any additional setup after loading the view.
+        var text = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 40))
+        text.text = "Hello"
     }
 
 // Testing 124
